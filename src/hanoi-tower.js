61b922e71d561a312b3be1300b0e48ab64349481
Andrey Sinaf <andre.as2016e@gmail.com>
2020-10-01 23:09:05 +0300

    test

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -1,6 +1,8 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function calculateHanoi(/* disksNumber, turnsSpeed */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function calculateHanoi(disksNumber, turnsSpeed) {
+  let t = 2 ** disksNumber - 1;
+  let s = Math.floor((t / turnsSpeed) * 60 * 60);
+  const obj = { turns: t, seconds: s };
+  return obj;
 };
--- src/what-season.js
+++ src/what-season.js
@@ -1,6 +1,34 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function getSeason(/* date */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function getSeason(date) {
+  if (!date) return "Unable to determine the time of year!";
+
+  let checkClass = (data) => ({}.toString.call(data).slice(8, -1));
+  if (checkClass(date) !== "Date") throw new Error();
+
+  let month = date.getMonth();
+  season = "";
+  switch (month) {
+    case 11:
+    case 0:
+    case 1:
+      season = "winter";
+      break;
+    case 2:
+    case 3:
+    case 4:
+      season = "spring";
+      break;
+    case 5:
+    case 6:
+    case 7:
+      season = "summer";
+      break;
+    case 8:
+    case 9:
+    case 10:
+      season = "fall";
+      break;
+  }
+  return season;
 };
